bd8396fef5593e19ea3fd1a8c6cc2807fb262e29
naokiwatari <nwatari@gmail.com>
2015-12-09 09:18:16 +0000

    before "rails g migration add_age_to_messages age:integer"

--- db/migrate/20151203143420_create_messages.rb
+++ db/migrate/20151203143420_create_messages.rb
@@ -2,7 +2,6 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.string :name
-      t.integer :age
       t.string :body
 
       t.timestamps null: false
